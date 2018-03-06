4cd882498f78da63d58cb74b1ef3b86186e0383d
Yiling Cao <yiling.cao@gmail.com>
2018-03-06 17:43:11 +0800

    fix string length overflow bug.

--- test_bip39.c
+++ test_bip39.c
@@ -8,14 +8,14 @@
 
 int main()
 {
-    const int64_t diff = 100000000000000LL;
+    unsigned long long int difficult = 1000000000000LL;
     uint8_t * m;
-    uint8_t * m2 = malloc(sizeof(uint8_t)*32);
-    unsigned char  * sk=malloc(sizeof(unsigned char)*32);
-    unsigned char  * pk=malloc(sizeof(unsigned char)*32);
-    uint8_t *sha256_1=malloc(sizeof(uint8_t)*32);
+    uint8_t * m2 = malloc(sizeof(uint8_t)*256);
+    unsigned char  * sk=malloc(sizeof(unsigned char)*256);
+    unsigned char  * pk=malloc(sizeof(unsigned char)*256);
+    uint8_t *sha256_1=malloc(sizeof(uint8_t)*256);
     int i,j;
-    int64_t lisk_addr;
+    unsigned long long int lisk_addr;
     char char_lisk_addr[30];
     char *msg=malloc(sizeof(char)*32);
 
@@ -34,8 +34,8 @@ int main()
         lisk_addr = strtoull(msg, NULL, 16);
         
 
-        sprintf(char_lisk_addr, "%lld", lisk_addr); 
-        if(strlen(char_lisk_addr)< 15){
+        if(lisk_addr< difficult){
+            sprintf(char_lisk_addr, "%lld", lisk_addr); 
             printf("%d  %lluL  %s\n", strlen(char_lisk_addr), lisk_addr, m);
         }
     }
